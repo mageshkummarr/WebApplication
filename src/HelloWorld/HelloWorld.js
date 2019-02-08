b84160af4853c3a9ad07d54cd9dc51ed64ff3d2d
sittichot <sittichot.c@gmail.com>
2019-02-08 12:14:49 +0700

    TestPropAndState

--- src/HelloWorld/HelloWorld.js
+++ src/HelloWorld/HelloWorld.js
@@ -2,10 +2,34 @@ import React, { Component } from 'react'
 import './HelloWorld.css'
 
 class HelloWorld extends Component{
+    constructor(props){
+        super(props);
+        this.main = this.main.bind(this);
+        this.state = {
+            text : '',
+            i : 0
+        }
+    }
+    main(){
+        if(this.state.i === 0){
+            this.setState(function(prevState){
+                return{
+                    text : 'Hello'
+                }
+            });
+        }else{
+            this.setState(function(prevState){
+                return{
+                    text : 'Welcome'
+                }
+            });
+        }
+    }
     render(){
         return(
             <div className='HelloWorld'>
-                <h2>Welcome Sittichot</h2>
+                <button onClick={this.main}>Click !</button>
+                <h2>{this.state.text} Sittichot</h2>
             </div>
         )
     }
